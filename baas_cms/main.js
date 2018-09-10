07a77d3c399aee13ec86db9c634ba0ed1a21504e
yinzhou <52643161@qq.com>
2018-09-11 00:07:24 +0800

    完善baas_cms

--- baas_cms/main.js
+++ baas_cms/main.js
@@ -1,13 +1,12 @@
 var express = require('express');
 var app = express();
 
-app.set('views', './views');
+app.set('views', `${__dirname}/views`);
 app.set('view engine', 'ejs');
 
+app.use(express.static(`${__dirname}/public`));
 
-app.use(express.static('public'));
-
-var server = app.listen(3000, function () {
+var server = app.listen(8000, function () {
     var host = server.address().address;
     var port = server.address().port;
 
