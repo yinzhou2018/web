4a7a8c1d3daeebfa804b1d53bd78d8cffc859e61
yinzhou <yinzhou@tencent.com>
2018-09-30 17:16:31 +0800

    参数调整

--- baas_cms/db/db_config.js
+++ baas_cms/db/db_config.js
@@ -1,31 +1,5 @@
 const MysqlAdapter = require('./mysql_adapter');
 
-const development = {
-  config_db: {
-    host: 'pcqbtest.mdb.mig',
-    port: 15916,
-    user: 'writeuser',
-    password: 'fWs2Wg3j36qynafQ',
-    database: 'baas',
-    charset: 'utf8',
-    multipleStatements: true,
-    connectionLimit: 3,
-  },
-};
-
-const product = {
-  config_db: {
-    host: 'qbcloudctrl.mdb.mig',
-    port: 15849,
-    user: 'writeuser',
-    password: 'CRW8iBYGZiPVRftv',
-    database: 'baas',
-    charset: 'utf8',
-    multipleStatements: true,
-    connectionLimit: 3,
-  },
-};
-
 const local = {
   config_db: {
     user: 'root',
@@ -35,9 +9,4 @@ const local = {
   }
 }
 
-let configDB = process.env.DOCKER_ENV === 'formal' ? product.config_db : development.config_db;
-if (!process.env.IP) {
-  configDB = local.config_db;
-}
-
-module.exports = configDB;
+module.exports = local.config_db;
